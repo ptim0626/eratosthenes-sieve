c4c745c291def91652fd568763e40e962300561a
timothy <timothy0626@gmail.com>
2021-01-30 16:56:17 +0000

    Found a file with maximum sieve size?

--- main.f
+++ main.f
@@ -1,8 +1,10 @@
       USE eratos
 
       INTEGER*4 SIEVE(16983957)/16983957*0/, N, SSVE, NPRIME
+C     INTEGER*4 SIEVE(17895698)/17895698*0/, N, SSVE, NPRIME
       INTEGER*4 PARR(100000000)
       N = 2038074744
+C     N = 2147483600
 
 C     use this subroutine to set the size of SIEVE
       CALL SSIEVE(N, SSVE)
